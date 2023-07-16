3f71ce8b2356dc08bc5672199e1096d92da9c6ef
shintaronishimura <md.sakura0803@gmail.com>
2023-07-17 08:38:30 +0900

    pixabayを使って画像検索をできるように

--- src/ImageGrallery.jsx
+++ src/ImageGrallery.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
 import "./ImageGrallery.css";
 
-const ImageGrallery = () => {
+const ImageGrallery = ({fetchData}) => {
   return (
     <div>
         <div className='images-wrapper'>
-            <div className='image'>
-                <img src='https://pixabay.com/get/g2d520812b46ffd2698fd165406db3b15c8d5c530be5b879f1ce02b5f762d683bc18d31cafb53a63f023665a4c342e151de55b7226faed8a2a5e2e8da87d58367_1280.jpg' alt='' />
+            {fetchData.map((data) => (
+                <div className='image' key={data.id}>
+                <a href={data.pageURL} target="_blank" rel="noreferrer">
+                <img src={data.largeImageURL} 
+                alt='' />
+                </a>
             </div>
-            <div className='image'>
-                <img src='https://pixabay.com/get/g2d520812b46ffd2698fd165406db3b15c8d5c530be5b879f1ce02b5f762d683bc18d31cafb53a63f023665a4c342e151de55b7226faed8a2a5e2e8da87d58367_1280.jpg' alt='' />
-            </div>
-            <div className='image'>
-                <img src='https://pixabay.com/get/g2d520812b46ffd2698fd165406db3b15c8d5c530be5b879f1ce02b5f762d683bc18d31cafb53a63f023665a4c342e151de55b7226faed8a2a5e2e8da87d58367_1280.jpg' alt='' />
-            </div>
-            <div className='image'>
-                <img src='https://pixabay.com/get/g2d520812b46ffd2698fd165406db3b15c8d5c530be5b879f1ce02b5f762d683bc18d31cafb53a63f023665a4c342e151de55b7226faed8a2a5e2e8da87d58367_1280.jpg' alt='' />
-            </div>
-            <div className='image'>
-                <img src='https://pixabay.com/get/g2d520812b46ffd2698fd165406db3b15c8d5c530be5b879f1ce02b5f762d683bc18d31cafb53a63f023665a4c342e151de55b7226faed8a2a5e2e8da87d58367_1280.jpg' alt='' />
-            </div>
+            ))}
         </div>
     </div>
   )
